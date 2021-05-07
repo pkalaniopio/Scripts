5837c12ab2f8cf6cb36848c0abf210dd3f5c8d3d
pkalaniopio <54751170+pkalaniopio@users.noreply.github.com>
2021-05-06 21:17:25 -0700

    Update GO_parser_plotter_v1.2.py

--- GO_parser_plotter/GO_parser_plotter_v1.2.py
+++ GO_parser_plotter/GO_parser_plotter_v1.2.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-#######################################################################################################################
-# This GO_parser_plotter software is intended to take gene ontology (GO) enrichment data from a Novogene RNASeq,      #
-# parse thru, grab relevant columns, and create user-defined heatmaps (Salanga Lab specific)                          #
-# Created by: Phillip Kalaniopio; Last edited: 5/2/2021                                                               #
-#######################################################################################################################
-
 version = "1.2"
 print("You are using Phillip Kalaniopio's GO_parser_plotter version %s" % version)
 
